39994e5ac10ed6880f44e90a24346c567bbd00a6
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-09-19 21:18:07 +0300

    completed the assignment

--- lib/get.py
+++ lib/get.py
@@ -2,9 +2,13 @@ import requests
 import json
 
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
-
 response = requests.get(url)
 
 json_content = json.loads(response.text)
 
+# format the json in a more readable manner
 print(json.dumps(json_content, indent=4))
+
+# print(response.text)  
+# print(response.content)
+# print(json_content)
